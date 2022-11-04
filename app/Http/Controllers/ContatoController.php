c548c22a9d8093c98437c3d3b7b8abf8c65f1fd7
WescleySil <wescley.silva.cas@gmail.com>
2022-11-03 23:37:32 -0300

    feat: :zap: Salvando no banco de dados
    Salvando os dados do formulário no banco de dados

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request){
-        dd($request);
+        $contato = new SiteContato();
+        $contato->create($request->except('_token'));
         return view("site.contato");
     }
 }
